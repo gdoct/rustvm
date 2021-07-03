e148c0b71c85bf33c07d94a973a7648c91d7d933
guido <guido.docter@gmail.com>
2021-07-03 14:09:51 +0200

    change stack pointer to Byte

--- src/cpu.rs
+++ src/cpu.rs
@@ -4,18 +4,21 @@ use crate::memory::Memory;
 use crate::instructions::{parse_opcode};
 use std::io::{Error, ErrorKind};
 
+// helper function to convert two bytes into a word
 fn create_word(bytes: (Byte, Byte)) -> Word { ((bytes.0 as Word) << 8) | bytes.1 as Word }
 
+// simple cpu with an 8-bit accumulator, two 8-bit registers, a 16-bit program counter and an 8-bit stack pointer
 pub struct Cpu {
     memory: Memory,
     a : Byte,                   // A register
     x : Byte,                   // X register
     y : Byte,                   // Y register
     pc : Word,                  // Program counter
-    sp : Word,                  // Stack pointer
+    sp : Byte,                  // Stack pointer
     flags : [Bit; 8]            // Cpu flags
 }
 
+// default factory method
 impl Factory for Cpu {
     fn new() -> Self {
         Self { 
@@ -24,24 +27,15 @@ impl Factory for Cpu {
             x : 0, 
             y : 0, 
             pc : 0xFFFC, 
-            sp : 0x0100,
+            sp : 0xFF,
             flags : [false; 8]
         }
     }
 }
 
+// cpu control functions: run, step, reset, load rom
 impl CpuController for Cpu {
-    fn reset(&mut self) -> std::io::Result<()> {
-        self.a = 0;
-        self.x = 0;
-        self.y = 0;
-        self.pc = 0xFFFC;
-        self.sp = 0x0100;
-        for i in 0..7 { self.flags[i] = false }
-        self.memory.reset();
-        Ok(())
-    }
-
+    /// keep running until BRK is raised
     fn run(&mut self) -> std::io::Result<()> {
         while !self.flags[CpuFlags::BREAK] {
             self.step()?;
@@ -49,13 +43,27 @@ impl CpuController for Cpu {
         Ok(())
     }
 
+    // execute one instruction (2+ clock steps)
     fn step(&mut self) -> std::io::Result<()> {
         let opcode = self.fetch_byte()?;
         let instr = parse_opcode(opcode)?;
         instr.execute(self)?;
         Ok(())
     }
+    
+    // reset the machine, clear all registers and cpu flags and clear memory
+    fn reset(&mut self) -> std::io::Result<()> {
+        self.a = 0;
+        self.x = 0;
+        self.y = 0;
+        self.pc = 0xFFFC;
+        self.sp = 0xFF;
+        for i in 0..7 { self.flags[i] = false }
+        self.memory.reset();
+        Ok(())
+    }
 
+    // load data from a file into the specified address. the data must fit in available memory after <base_address>.
     fn load_rom(&mut self, data: &[Byte], len: usize, base_address: Word) -> std::io::Result<()> {
         let datalen = data.len();
         if len > datalen || len == 0 || datalen==0 { return Err(Error::new(ErrorKind::Other, "invalid program, length")) }
@@ -69,6 +77,7 @@ impl CpuController for Cpu {
     }
 }
 
+// these are all internal cpu operations which are called from the instructions structs.
 impl VirtualCpu for Cpu {
     fn fetch_byte(&mut self) -> std::io::Result<Byte> {
         let byte = self.memory.read(self.pc);
